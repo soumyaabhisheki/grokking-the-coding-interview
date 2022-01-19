f7473a6776a69c80e3bbb77e3635f99bd4fd0a25
soumyaabhisheki <96761539+soumyaabhisheki@users.noreply.github.com>
2022-01-19 15:50:37 -0600

    Slow and Fast Pointer

---
+++ Middle of the Linked List.py
@@ -0,0 +1,13 @@
+#  Given the head of a singly linked list, return the middle node of the linked list.
+#
+# If there are two middle nodes, return the second middle node.
+#  Input: head = [1,2,3,4,5]
+# Output: [3,4,5]
+# Explanation: The middle node of the list is node 3.
+
+def middleNode(self, head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
