cecde5a97915c1ad2a5f6daf367c5f23725f6f13
soumyaabhisheki <96761539+soumyaabhisheki@users.noreply.github.com>
2022-02-09 19:15:58 -0600

    Cyclic Sortr

---
+++ Find the Duplicate Number.py
@@ -0,0 +1,23 @@
+#  We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’.
+# The array has only one duplicate but it can be repeated multiple times.
+# Find that duplicate number without using any extra space. You are, however, allowed to modify the input array.
+# Example 1:
+# Input: [1, 4, 4, 3, 2]
+# Output: 4
+# Example 2:
+# Input: [2, 1, 3, 3, 5, 4]
+# Output: 3
+
+def find_duplicate(nums):
+    i = 0
+    while i < len(nums):
+        if nums[i] != i + 1:
+            j = nums[i] - 1
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]  # swap
+            else:  # we have found the duplicate
+                return nums[i]
+        else:
+            i += 1
+
+    return -1
